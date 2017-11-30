74a8cfae5c38e7e04fe54acca5e278fa8c9b3c35
AlexREvans <AlexREvans@users.noreply.github.com>
2017-11-30 21:51:06 +0000

    Tidy up example matching

--- src/data/aggregator.js
+++ src/data/aggregator.js
@@ -2,7 +2,6 @@ var rawData = require('./finhub.js');
 var levenshtein = require('fast-levenshtein');
 
 var examples = rawData.examples;
-var threshold = 10;
 
 var similarityPercentage = (a,b) => {
     var longest = Math.max(a.length, b.length);
@@ -10,31 +9,37 @@ var similarityPercentage = (a,b) => {
     return Math.floor((longest - distance) * 100 / longest);
 };
 
+var constructResult = (example, similarity, transaction) => ({
+    tag: example.class,
+    matchingExample: {example, similarity},
+    transaction
+});
+
+var findClosestExample = (examples, transaction, threshold = 10) => examples
+    .map(example => ({
+        example, 
+        similarity: similarityPercentage(example.name, transaction.name)
+    }))
+    .reduce(
+        (a, b) => (a.similarity > b.similarity ? a : b), 
+        {example: null, similarity: threshold});
+
 
 var results = rawData.transactions.map(transaction => {
 
     if(transaction.example) {
-        return {
-            tag: transaction.class,
-            matchingExample: {example: transaction, similarity: 100},
-            transaction
-        };
+        return constructResult(
+            transaction, 
+            100, 
+            transaction);
     }
 
-    var closestExample = examples
-        .map(example => ({
-            example, 
-            similarity: similarityPercentage(example.name, transaction.name)
-        }))
-        .reduce(
-            (a, b) => (a.similarity > b.similarity ? a : b), 
-            {example: null, similarity: threshold});
-
-    return {
-        tag: closestExample.example.class, 
-        matchingExample: closestExample,
-        transaction: transaction
-    };
+    var closestExample = findClosestExample(examples, transaction);
+    
+    return constructResult(
+        closestExample.example, 
+        closestExample.similarity, 
+        transaction);
 });
 
 module.exports = results;
